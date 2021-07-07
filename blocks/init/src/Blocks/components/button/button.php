43f33a84b59e976dee73cff47720399effb41450
Karlo Biscan <karlob@pointjupiter.com>
2021-07-07 15:45:20 +0200

    Update button component

--- blocks/init/src/Blocks/components/button/button.php
+++ blocks/init/src/Blocks/components/button/button.php
@@ -8,28 +8,32 @@
 
 use EightshiftBoilerplateVendor\EightshiftLibs\Helpers\Components;
 
-$globalManifest = Components::getManifest(dirname(__DIR__, 2));
 $manifest = Components::getManifest(__DIR__);
+$componentName = $attributes['componentName'] ?? $manifest['componentName'];
 
-$buttonUse = Components::checkAttr('buttonUse', $attributes, $manifest);
+$buttonUse = Components::checkAttr('buttonUse', $attributes, $manifest, $componentName);
 if (!$buttonUse) {
 	return;
 }
-
-$unique = Components::getUnique();
-echo Components::outputCssVariables($attributes, $manifest, $unique, $globalManifest); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 $componentClass = $attributes['componentClass'] ?? $manifest['componentClass'];
 $selectorClass = $attributes['selectorClass'] ?? $componentClass;
 $blockClass = $attributes['blockClass'] ?? '';
 
-$buttonUrl = Components::checkAttr('buttonUrl', $attributes, $manifest);
-$buttonContent = Components::checkAttr('buttonContent', $attributes, $manifest);
-$buttonIsAnchor = Components::checkAttr('buttonIsAnchor', $attributes, $manifest);
-$buttonId = Components::checkAttr('buttonId', $attributes, $manifest);
-$buttonIsNewTab = Components::checkAttr('buttonIsNewTab', $attributes, $manifest);
-$buttonAriaLabel = Components::checkAttr('buttonAriaLabel', $attributes, $manifest);
-$buttonAttrs = Components::checkAttr('buttonAttrs', $attributes, $manifest);
+$buttonUrl = Components::checkAttr('buttonUrl', $attributes, $manifest, $componentName);
+$buttonContent = Components::checkAttr('buttonContent', $attributes, $manifest, $componentName);
+$buttonIsAnchor = Components::checkAttr('buttonIsAnchor', $attributes, $manifest, $componentName);
+$buttonId = Components::checkAttr('buttonId', $attributes, $manifest, $componentName);
+$buttonJsClass = Components::checkAttr('buttonJsClass', $attributes, $manifest, $componentName);
+$buttonIsNewTab = Components::checkAttr('buttonIsNewTab', $attributes, $manifest, $componentName);
+$buttonAriaLabel = Components::checkAttr('buttonAriaLabel', $attributes, $manifest, $componentName);
+$buttonAttrs = Components::checkAttr('buttonAttrs', $attributes, $manifest, $componentName);
+$buttonAlign = Components::checkAttr('buttonAlign', $attributes, $manifest, $componentName);
+$buttonColor = Components::checkAttr('buttonColor', $attributes, $manifest, $componentName);
+$buttonSize = Components::checkAttr('buttonSize', $attributes, $manifest, $componentName);
+$buttonWidth = Components::checkAttr('buttonWidth', $attributes, $manifest, $componentName);
+$buttonIconPosition = Components::checkAttr('buttonIconPosition', $attributes, $manifest, $componentName);
+$iconUse = Components::checkAttr('iconUse', $attributes, $manifest, $componentName);
 
 if ($buttonIsNewTab) {
 	$buttonAttrs = array_merge(
@@ -39,46 +43,83 @@ if ($buttonIsNewTab) {
 		],
 		$buttonAttrs
 	);
-}
+};
+
+$buttonWrapClass = Components::classnames([
+	Components::selector($componentClass, $componentClass, 'wrap'),
+	Components::selector($buttonAlign, $componentClass, 'align', $buttonAlign),
+	Components::selector($blockClass, $blockClass, "{$selectorClass}-wrap"),
+]);
 
 $buttonClass = Components::classnames([
 	$componentClass,
-	Components::selector($buttonIsAnchor, 'js-scroll-to-anchor'),
+	Components::selector($buttonColor, $componentClass, 'color', $buttonColor),
+	Components::selector($buttonSize, $componentClass, 'size', $buttonSize),
+	Components::selector($buttonWidth, $componentClass, 'size-width', $buttonWidth),
+	Components::selector($buttonIsAnchor, $buttonIsAnchor, 'js-scroll-to-anchor'),
+	Components::selector($iconUse, $selectorClass, '', 'has-icon'),
+	Components::selector(($iconUse && $buttonIconPosition), $componentClass, '', "icon-position-{$buttonIconPosition}"),
 	Components::selector($blockClass, $blockClass, $selectorClass),
+	$buttonJsClass,
+]);
+
+$buttonContentClass = Components::classnames([
+	Components::selector($buttonContent, $componentClass, 'content')
+]);
+
+$iconSelectorClass = Components::classnames([
+	Components::selector($iconUse, $componentClass, 'icon')
 ]);
 
 ?>
 
-<?php if (! $buttonUrl) { ?>
-	<button
-		class="<?php echo \esc_attr($buttonClass); ?>"
-		id="<?php echo \esc_attr($buttonId); ?>"
-		title="<?php echo \esc_attr($buttonContent); ?>"
-		aria-label="<?php echo \esc_attr($buttonAriaLabel); ?>"
-		data-id="<?php echo esc_attr($unique); ?>"
-		<?php
-		foreach ($buttonAttrs as $key => $value) {
-			echo \wp_kses_post("{$key}=" . $value . " ");
-		}
-		?>
-	>
-		<?php echo \esc_html($buttonContent); ?>
-	</button>
-
-<?php } else { ?>
-	<a
-		href="<?php echo \esc_url($buttonUrl); ?>"
-		class="<?php echo \esc_attr($buttonClass); ?>"
-		id="<?php echo \esc_attr($buttonId); ?>"
-		title="<?php echo \esc_attr($buttonContent); ?>"
-		aria-label="<?php echo \esc_attr($buttonAriaLabel); ?>"
-		data-id="<?php echo esc_attr($unique); ?>"
-		<?php
-		foreach ($buttonAttrs as $key => $value) {
-			echo \wp_kses_post("{$key}=" . $value . " ");
-		}
-		?>
-	>
-		<?php echo \esc_html($buttonContent); ?>
-	</a>
-<?php } ?>
+<div class="<?php echo \esc_attr($buttonWrapClass); ?>">
+	<?php if (!$buttonUrl) { ?>
+		<button class="<?php echo \esc_attr($buttonClass); ?>" id="<?php echo \esc_attr($buttonId); ?>" title="<?php echo \esc_attr($buttonContent); ?>" aria-label="<?php echo \esc_attr($buttonAriaLabel); ?>" <?php
+																																																					foreach ($buttonAttrs as $key => $value) {
+																																																						echo "{$key}=" . $value . " ";
+																																																					}
+																																																					?>>
+			<?php echo
+			Components::render(
+				'icon',
+				array_merge(
+					$attributes,
+					[
+						'componentClass' => 'icon',
+						'blockClass' => 'btn',
+						'selectorClass' => 'icon'
+					]
+				)
+			); ?>
+			<?php if ($buttonContent && $buttonIconPosition != 'center') { ?>
+				<span class="<?php echo \esc_attr($buttonContentClass); ?>">
+					<?php echo \esc_html($buttonContent); ?>
+				</span>
+			<?php } ?>
+		</button>
+	<?php } else { ?>
+		<a href="<?php echo \esc_url($buttonUrl); ?>" class="<?php echo \esc_attr($buttonClass); ?>" id="<?php echo \esc_attr($buttonId); ?>" title="<?php echo \esc_attr($buttonContent); ?>" aria-label="<?php echo \esc_attr($buttonAriaLabel); ?>" <?php foreach ($buttonAttrs as $key => $value) {
+																																																															echo \wp_kses_post("{$key}=" . $value . " ");
+																																																														} ?>>
+			<?php
+			echo Components::render(
+				'icon',
+				array_merge(
+					$attributes,
+					[
+						'componentClass' => 'icon',
+						'blockClass' => 'btn',
+						'selectorClass' => 'icon'
+					]
+				)
+			);
+			?>
+			<?php if ($buttonContent && $buttonIconPosition != 'center') { ?>
+				<span class="<?php echo \esc_attr($buttonContentClass); ?>">
+					<?php echo \esc_html($buttonContent); ?>
+				</span>
+			<?php } ?>
+		</a>
+	<?php } ?>
+</div>
